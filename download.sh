a7dcd282ae73f1a6919a1a11cc108da9f1926290
Houssem Ben Ali <hbenali@exoplatform.com>
2021-03-04 18:18:22 +0100

    Allow to retrieve releases

--- download.sh
+++ download.sh
@@ -2,5 +2,5 @@
 echo "Downloading Jitsi Call App version ${JITSI_CALL_VERSION}"
 GROUP_ID=org.exoplatform
 ARTIFACT_ID=jitsi-call
-DOWNLOAD_URL="https://repository.exoplatform.org/service/local/artifact/maven/redirect?r=exo-addons-snapshots&g=${GROUP_ID}&a=${ARTIFACT_ID}&v=${JITSI_CALL_VERSION}"
+[[ ${JITSI_CALL_VERSION} =~ .*x-SNAPSHOT$ ]] DOWNLOAD_URL="https://repository.exoplatform.org/service/local/artifact/maven/redirect?r=exo-addons-snapshots&g=${GROUP_ID}&a=${ARTIFACT_ID}&v=${JITSI_CALL_VERSION}" || DOWNLOAD_URL="https://repository.exoplatform.org/service/local/artifact/maven/redirect?r=exo-addons-releases&g=${GROUP_ID}&a=${ARTIFACT_ID}&v=${JITSI_CALL_VERSION}"
 wget -O /jitsi-call.jar ${DOWNLOAD_URL}
